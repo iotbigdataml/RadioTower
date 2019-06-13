e604f7e19b206067699b5034a7a33e759c562662
atumu <atumu@andrew.cmu.edu>
2019-06-12 22:14:19 -0400

    added radio read write code

---
+++ Radio_readwrite/Radio_readwrite.ino
@@ -0,0 +1,162 @@
+
+#include <SoftwareSerial.h>
+#include <SPI.h>
+//#include <nRF24L01.h>
+#include <RF24.h>
+
+#define RX 9                            // Pin used for the receive port on the Arduino
+#define TX 10                           // Pin used for the transmit port on the Arduino
+#define MsgLn 25                        // Max size of a message
+
+char character;                         // Character read from the serial port
+SoftwareSerial mySerial(RX, TX, true);  // Here we define the serial port object
+
+
+RF24 radio(7, 8);                       // Chip enable (7), Chip Select (8)
+const byte address1[6] = "01011";        // Radio address - use only the channels that match the
+                                        // numbers on your robots.
+const byte address2[6] = "01100";        // Radio address - use only the channels that match the
+
+char Msg[MsgLn];                        // Message buffer
+int i;                                  // Loop index
+
+void setup() 
+{
+  Serial.begin(9600);                   // Debug output in case you are connected to a monitor
+  radio.begin();                        // Instantiate the radio object
+    
+  if (!radio.isChipConnected())
+  {
+      Serial.print("RADIO NOT CONNECTED!");
+      while(!radio.isChipConnected());
+  }
+  
+//  radio.openWritingPipe(address);       // Open the radio pipe using your address (read about pipes and channels)
+//  radio.setPALevel(RF24_PA_MIN);        // Set the power level. Since the bots and the radio base station are close I use min power
+//  radio.stopListening();                // Now we listen for messages...
+//  Serial.println("Radio Ready...");
+
+  mySerial.begin(9600);                 // Open the software serial port. Once we open the port we
+                                        // print a message and wait a second for things to settle
+                                        // down.
+                        
+  Serial.println("Port setup complete...");
+  delay(1000);
+
+  i = 0;
+}
+
+void loop()                            
+{
+
+  while(mySerial.available())
+  {
+     character = mySerial.read();   // Here is where we read the data
+      
+     if (character == '+')          // Note that a '+' is a marker for the end of the message
+     {
+      
+        if(!strcmp(Msg,"q"))
+        {
+            radio.openWritingPipe(address1);       // Open the radio pipe using your address (read about pipes and channels)
+            radio.setPALevel(RF24_PA_MIN);        // Set the power level. Since the bots and the radio base station are close I use min power
+            radio.stopListening();                // Now we listen for messages...
+          Serial.println("Moving bot11");
+          Serial.println(Msg);
+          radio.write(&Msg, i);       // Here with send the message
+          radio.openReadingPipe(0,address1);
+          radio.startListening();
+          delay(100);
+          if (radio.available())              // If we have messages, we print them out - otherwise we do nothing but listen.
+           {
+            char text[32] = "";
+            radio.read(&text, sizeof(text));
+            Serial.println(text);
+           }
+           radio.stopListening();
+          
+
+
+
+        }
+        else if(!strcmp(Msg,"p"))
+        {
+          radio.openWritingPipe(address2);       // Open the radio pipe using your address (read about pipes and channels)
+            radio.setPALevel(RF24_PA_MIN);        // Set the power level. Since the bots and the radio base station are close I use min power
+            radio.stopListening();                // Now we listen for messages...
+          Serial.println("Moving bot12");
+          Serial.println(Msg);
+           radio.write(&Msg, i); 
+            radio.openReadingPipe(0,address1);
+            radio.startListening();
+            delay(100);
+            if (radio.available())              // If we have messages, we print them out - otherwise we do nothing but listen.
+            {
+              char text[32] = "";
+              radio.read(&text, sizeof(text));
+              Serial.println(text);
+            }
+            radio.stopListening();
+
+        }
+        else if(!strcmp(Msg,"m"))
+        {
+          Serial.println("Maintenance mode");
+//           mySerial.write("The quick red fox jumped over the lazy brown dog.\n");
+            radio.openWritingPipe(address1);       // Open the radio pipe using your address (read about pipes and channels)
+            radio.setPALevel(RF24_PA_MIN);        // Set the power level. Since the bots and the radio base station are close I use min power
+            radio.stopListening();                // Now we listen for messages...
+            radio.write(&Msg, i);
+            Serial.println(Msg);
+            radio.openWritingPipe(address2);       // Open the radio pipe using your address (read about pipes and channels)
+            radio.setPALevel(RF24_PA_MIN);        // Set the power level. Since the bots and the radio base station are close I use min power
+            radio.stopListening();                // Now we listen for messages...
+            radio.write(&Msg, i);
+            Serial.println(Msg);
+
+            radio.openReadingPipe(0,address1);
+            radio.startListening();
+            delay(100);
+            if (radio.available())              // If we have messages, we print them out - otherwise we do nothing but listen.
+            {
+              char text[32] = "";
+              radio.read(&text, sizeof(text));
+              Serial.println(text);
+            }
+            radio.stopListening();
+            radio.openReadingPipe(0,address2);
+            radio.startListening();
+            delay(100);
+            if (radio.available())              // If we have messages, we print them out - otherwise we do nothing but listen.
+            {
+              char text[32] = "";
+              radio.read(&text, sizeof(text));
+              Serial.println(text);
+            }
+            radio.stopListening();
+
+        }
+//        else if(!strcmp(Msg,"s"))
+//        {
+//          Serial.println("Moving Backward");
+////            mySerial.write("The quick red fox jumped over the lazy brown dog.\n");
+//            radio.write(&Msg, i); 
+//
+//        }
+//        else
+//        {
+//          Serial.print("Input Provided is not part of the commands! Input:");
+//          Serial.println(Msg);        // Debug
+//        }
+        memset(Msg,0,MsgLn);
+        i = 0;
+     }
+     else
+     {
+        Msg[i] = character;         // Here we build the message
+        i++;  
+     } 
+       
+  } // while
+}
+  
